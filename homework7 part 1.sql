587c39664da8418d52d6df620f99d33606f1fb51
garlotardey <garlo.tardey@slu.edu>
2024-10-11 15:01:55 -0500

    Add files via upload

--- homework7 part 1.sql
+++ homework7 part 1.sql
@@ -6,32 +6,31 @@ order by first_name asc;
 -- 2
 select *
 from rental
-where return_date between '2005-05-28' AND '2005-06-01';
+where return_date between '2005-05-28' and '2005-06-01';
 
 -- 3
-SELECT customer.customer_id, customer.first_name AS customer_first_name, customer.last_name AS customer_last_name, SUM(payment.amount) AS total_spent
-FROM customer
-INNER JOIN rental ON customer.customer_id = rental.customer_id
-INNER JOIN payment ON rental.rental_id = payment.rental_id
-GROUP BY customer.customer_id, customer.first_name, customer.last_name
-ORDER BY total_spent ASC;
-
+select customer.customer_id, customer.first_name as customer_first_name, customer.last_name as customer_last_name, sum(payment.amount) as total_spent
+from customer
+inner join rental on customer.customer_id = rental.customer_id
+inner join payment on rental.rental_id = payment.rental_id
+group by customer.customer_id, customer.first_name, customer.last_name
+order by total_spent asc;
 
 -- 4
-SELECT first_name, last_name, SUM(payment.amount) AS total_spent
-FROM customer
-INNER JOIN payment  ON customer.customer_id = payment.customer_id
-GROUP BY customer.customer_id, customer.first_name, customer.last_name
-ORDER BY total_spent ASC;
+select first_name, last_name, sum(payment.amount) as total_spent
+from customer
+inner join payment  on customer.customer_id = payment.customer_id
+group by customer.customer_id, customer.first_name, customer.last_name
+order by total_spent asc;
 
 -- 5
-SELECT actor.actor_id, actor.first_name AS actor_first_name, actor.last_name AS actor_last_name, COUNT(film.film_id) AS film_count
-FROM actor 
-INNER JOIN film_actor  ON actor.actor_id = film_actor.actor_id
-INNER JOIN film ON film_actor.film_id = film.film_id
-WHERE YEAR (film.release_year) = 2006
-GROUP BY actor.actor_id, actor.first_name, actor.last_name
-ORDER BY film_count DESC;
+select actor.actor_id, actor.first_name as actor_first_name, actor.last_name as actor_last_name, count(film.film_id) as film_count
+from actor 
+inner join film_actor  on actor.actor_id = film_actor.actor_id
+inner join film on film_actor.film_id = film.film_id
+where year (film.release_year) = 2006
+group by actor.actor_id, actor.first_name, actor.last_name
+order by film_count desc;
 
 --6 
 -- 4 in 4 first name and last name are selected from customer sum(payment.amount) finds the sum of payment in amount
@@ -43,19 +42,19 @@ ORDER BY film_count DESC;
 
 
 -- 7
-SELECT category.name, AVG(film.rental_rate) AS average_rental_rate
-FROM category 
-INNER JOIN film_category  ON category.category_id = film_category.category_id
-INNER JOIN film ON film_category.film_id = film.film_id
-GROUP BY category.name
-ORDER BY average_rental_rate;
+select category.name, avg(film.rental_rate) as average_rental_rate
+from category 
+inner join film_category  on category.category_id = film_category.category_id
+inner join film on film_category.film_id = film.film_id
+group by category.name
+order by average_rental_rate;
 --8
-SELECT category.category_id, category.name, COUNT(rental.rental_id) AS rental_count, SUM(payment.amount) AS total_sales
-FROM category 
-INNER JOIN film_category ON category.category_id = film_category.category_id
-INNER JOIN film ON film_category.film_id = film.film_id
-INNER JOIN rental ON film.film_id = rental.rental_id
-INNER JOIN payment ON rental.rental_id = payment.rental_id
-GROUP BY category.category_id, category.name
-ORDER BY rental_count DESC
-LIMIT 5;
+select category.category_id, category.name, count(rental.rental_id) as rental_count, sum(payment.amount) as total_sales
+from category 
+inner join film_category on category.category_id = film_category.category_id
+inner join film on film_category.film_id = film.film_id
+inner join rental on film.film_id = rental.rental_id
+inner join payment on rental.rental_id = payment.rental_id
+group by category.category_id, category.name
+order by rental_count desc
+limit 5;
